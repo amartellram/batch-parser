364e271a0dcc38aeb82b2d71c338a5c639863cb7
Abel Martell <amartellram@gmail.com>
2022-12-13 21:31:11 -0500

    first commit

---
+++ src/main/java/io/amartell/batch/parser/BatchParserApplication.java
@@ -0,0 +1,13 @@
+package io.amartell.batch.parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/amartell/batch/parser/BatchParserApplicationTests.java
@@ -0,0 +1,13 @@
+package io.amartell.batch.parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

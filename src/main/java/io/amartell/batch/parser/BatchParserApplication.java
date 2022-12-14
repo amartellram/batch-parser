6834a5527c8cc4381f5490abb86b15409938c159
Abel Martell <amartellram@gmail.com>
2022-12-13 22:09:28 -0500

    added initial sample

--- src/main/java/io/amartell/batch/parser/BatchParserApplication.java
+++ src/main/java/io/amartell/batch/parser/BatchParserApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BatchParserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BatchParserApplication.class, args);
-	}
+  public static void main(String[] args) {
+    System.exit(SpringApplication.exit(SpringApplication.run(BatchParserApplication.class, args)));
+  }
 
 }
